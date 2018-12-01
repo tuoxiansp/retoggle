370ae3de5d558a6ac6e1fcfe4afac49edeaf563e
b1ncer <b1ncer@foxmail.com>
2018-12-01 17:05:34 +0800

    quick fix removeKnob .

--- src/example/app.tsx
+++ src/example/app.tsx
@@ -1,64 +1,66 @@
-import React from "react";
-import styled from "styled-components";
-import { Ghost } from "react-kawaii";
+import React from 'react'
+import styled from 'styled-components'
+import { Ghost } from 'react-kawaii'
 import {
-  Inspector,
-  useLog,
-  useBooleanKnob,
-  useNumberKnob,
-  useRangeKnob,
-  useSelectKnob,
-  useTimeMachine,
-  useColorKnob,
-  useObjectKnob
-} from "../lib";
-import useChartKnob from "./custom-chart-knob";
+    Inspector,
+    useLog,
+    useBooleanKnob,
+    useNumberKnob,
+    useRangeKnob,
+    useSelectKnob,
+    useTimeMachine,
+    useColorKnob,
+    useObjectKnob,
+} from '../lib'
+import useChartKnob from './custom-chart-knob'
 
 const Tile = styled.div`
-  background-color: #3356d0;
-  flex-direction: column;
-  display: flex;
-  padding: 15px;
-  margin: 5px;
-  border-radius: 5px;
-  width: 400px;
-  height: 400px;
-  align-items: center;
-  justify-content: center;
+    background-color: #3356d0;
+    flex-direction: column;
+    display: flex;
+    padding: 15px;
+    margin: 5px;
+    border-radius: 5px;
+    width: 400px;
+    height: 400px;
+    align-items: center;
+    justify-content: center;
 
-  box-shadow: rgba(195, 22, 22, 0.03) 0px 0px 5px,
-    rgba(63, 81, 181, 0.55) 10px 13px 47px;
-`;
+    box-shadow: rgba(195, 22, 22, 0.03) 0px 0px 5px, rgba(63, 81, 181, 0.55) 10px 13px 47px;
+`
 
-export default function Icon() {
-  const [visibility] = useBooleanKnob("Icon Visibility", true);
+function Icon() {
+    const [ visibility ] = useBooleanKnob('Icon Visibility', true)
 
-  useLog("Visibility", {
-    visibility
-  });
+    useLog('Visibility', {
+        visibility,
+    })
 
-  const [borderWidth] = useNumberKnob("Border Width");
-  const [rangeKnobValue] = useRangeKnob("Ghost Size", {
-    initialValue: 240,
-    min: 0,
-    max: 500
-  });
-  const [mood] = useSelectKnob(
-    "Mood",
-    ["sad", "shocked", "happy", "blissful", "lovestruck", "excited"],
-    "blissful"
-  );
-  const [colorValue] = useColorKnob("Color", "wheat");
-  const [obj, setObj] = useObjectKnob("Object", {});
-  const timeMachineValue = useTimeMachine("Color timemachine", colorValue);
-  useChartKnob("Chart", timeMachineValue);
+    const [ borderWidth ] = useNumberKnob('Border Width')
+    const [ rangeKnobValue ] = useRangeKnob('Ghost Size', {
+        initialValue: 240,
+        min: 0,
+        max: 500,
+    })
+    const [ mood ] = useSelectKnob(
+        'Mood',
+        [ 'sad', 'shocked', 'happy', 'blissful', 'lovestruck', 'excited' ],
+        'blissful'
+    )
+    const [ colorValue ] = useColorKnob('Color', 'wheat')
+    const [ obj, setObj ] = useObjectKnob('Object', {})
+    const timeMachineValue = useTimeMachine('Color timemachine', colorValue)
+    useChartKnob('Chart', timeMachineValue)
 
-  return (
-    <Tile style={{ borderWidth, borderStyle: "solid", borderColor: "white" }}>
-      <Inspector />
-      {visibility && (
-        <Ghost size={rangeKnobValue} mood={mood} color={timeMachineValue} />
-      )}
-    </Tile>
-  );
+    return (
+        <Tile style={{ borderWidth, borderStyle: 'solid', borderColor: 'white' }}>
+            {visibility && <Ghost size={rangeKnobValue} mood={mood} color={timeMachineValue} />}
+        </Tile>
+    )
+}
+
+export default function App() {
+    const [ useIcon ] = useBooleanKnob('Use Icon', true)
+
+    return useIcon ? <Icon /> : <div>123abc</div>
 }
--- src/lib/inspector/state-handler.ts
+++ src/lib/inspector/state-handler.ts
@@ -1,90 +1,94 @@
 class StateHandler {
-  public logs: any;
-  public knobs: any;
-  public knobRenderers: any = {};
+    public logs: any
+    public knobs: any
+    public knobRenderers: any = {}
 
-  public logSubscriber: (log: any) => void;
-  public knobSubscriber: (knob: any) => void;
+    public logSubscriber: (log: any) => void
+    public knobSubscriber: (knob: any) => void
 
-  public addLogSubscriber = (subscriber: any) => {
-    this.logSubscriber = subscriber;
-  };
-
-  public addKnobSubscriber = (subscriber: any) => {
-    this.knobSubscriber = subscriber;
-  };
-
-  public haveLogs = () => {
-    return !!this.logs;
-  };
-
-  public haveKnobs = () => {
-    return !!this.knobs;
-  };
-
-  public getLogs = () => {
-    return this.logs;
-  };
-
-  public getKnobs = () => {
-    return this.knobs;
-  };
-
-  public setLog = (log: any) => {
-    if (this.logSubscriber) {
-      this.logSubscriber(log);
+    public addLogSubscriber = (subscriber: any) => {
+        this.logSubscriber = subscriber
     }
 
-    this.logs = {
-      ...this.logs,
-      [log.name]: log
-    };
-  };
-
-  public removeLog = (name: string) => {
-    if (this.logs) {
-      delete this.logs[name];
-    }
-  };
-
-  public setKnob = (knob: any) => {
-    if (this.knobSubscriber) {
-      this.knobSubscriber(knob);
+    public addKnobSubscriber = (subscriber: any) => {
+        this.knobSubscriber = subscriber
     }
 
-    this.knobs = {
-      ...this.knobs,
-      [knob.name]: knob
-    };
-  };
-
-  public removeKnob = (name: string) => {
-    if (this.knobs) {
-      delete this.knobs[name];
+    public haveLogs = () => {
+        return !!this.logs
     }
-  };
 
-  public addKnobRenderer = (name: string, component: any) => {
-    this.knobRenderers[name] = component;
-  };
+    public haveKnobs = () => {
+        return !!this.knobs
+    }
 
-  public getKnobRenderer = (name: string) => {
-    return this.knobRenderers[name];
-  };
+    public getLogs = () => {
+        return this.logs
+    }
+
+    public getKnobs = () => {
+        return this.knobs
+    }
+
+    public setLog = (log: any) => {
+        if (this.logSubscriber) {
+            this.logSubscriber(log)
+        }
+
+        this.logs = {
+            ...this.logs,
+            [log.name]: log,
+        }
+    }
+
+    public removeLog = (name: string) => {
+        if (this.logs) {
+            delete this.logs[name]
+        }
+    }
+
+    public setKnob = (knob: any) => {
+        if (this.knobSubscriber) {
+            this.knobSubscriber(knob)
+        }
+
+        this.knobs = {
+            ...this.knobs,
+            [knob.name]: knob,
+        }
+    }
+
+    public removeKnob = (name: string) => {
+        if (this.knobSubscriber) {
+            this.knobSubscriber(name)
+        }
+
+        if (this.knobs) {
+            delete this.knobs[name]
+        }
+    }
+
+    public addKnobRenderer = (name: string, component: any) => {
+        this.knobRenderers[name] = component
+    }
+
+    public getKnobRenderer = (name: string) => {
+        return this.knobRenderers[name]
+    }
 }
 
 // singleton
-const stateHandler = new StateHandler();
+const stateHandler = new StateHandler()
 
-export const addLogSubscriber = stateHandler.addLogSubscriber;
-export const addKnobSubscriber = stateHandler.addKnobSubscriber;
-export const haveLogs = stateHandler.haveLogs;
-export const haveKnobs = stateHandler.haveKnobs;
-export const getLogs = stateHandler.getLogs;
-export const getKnobs = stateHandler.getKnobs;
-export const setLog = stateHandler.setLog;
-export const removeLog = stateHandler.removeLog;
-export const setKnob = stateHandler.setKnob;
-export const removeKnob = stateHandler.removeKnob;
-export const addKnobRenderer = stateHandler.addKnobRenderer;
-export const getKnobRenderer = stateHandler.getKnobRenderer;
+export const addLogSubscriber = stateHandler.addLogSubscriber
+export const addKnobSubscriber = stateHandler.addKnobSubscriber
+export const haveLogs = stateHandler.haveLogs
+export const haveKnobs = stateHandler.haveKnobs
+export const getLogs = stateHandler.getLogs
+export const getKnobs = stateHandler.getKnobs
+export const setLog = stateHandler.setLog
+export const removeLog = stateHandler.removeLog
+export const setKnob = stateHandler.setKnob
+export const removeKnob = stateHandler.removeKnob
+export const addKnobRenderer = stateHandler.addKnobRenderer
+export const getKnobRenderer = stateHandler.getKnobRenderer
